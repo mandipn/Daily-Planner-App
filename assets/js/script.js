799e15d79a5e4f07f9899e3ec4b3d862b3830d2b
Mandip Nijor <mandip.nijor@gmail.com>
2023-02-19 12:52:57 +0000

    committing the changes from yesterday

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,20 @@
 $(document).ready(function () {
-    
+ 
+  $("#0900AM .itemforthehour").val(localStorage.getItem("0900AM"));
+  $("#1000AM .itemforthehour").val(localStorage.getItem("10h"));
+  $("#1100AM .itemforthehour").val(localStorage.getItem("11h"));
+  $("#1200PM .itemforthehour").val(localStorage.getItem("12h"));
+  $("#1300PM .itemforthehour").val(localStorage.getItem("13h"));
+  $("#1400PM .itemforthehour").val(localStorage.getItem("14h"));
+  $("#1500PM .itemforthehour").val(localStorage.getItem("15h"));
+  $("#1600PM .itemforthehour").val(localStorage.getItem("16h"));
+  $("#1700PM .itemforthehour").val(localStorage.getItem("17h"));  
+
     let LiveMoment = moment().format("dddd MMMM Do");
     let TodayDate = document.getElementById("today");
     TodayDate.innerHTML = LiveMoment;
-    let liveHour = moment().format("HH");
+    let liveHour = parseInt(moment().format("H"));
+    var timeBlock = $(".1hourcolumn");
 
  
  $("#resetInfoButton").click(function (event) {
@@ -12,36 +23,70 @@ $(document).ready(function () {
     localStorage.clear();
   });
 
-  $(".timeinrow").each(function () {
-    var hourdiv = $(this).attr("id").split("-")[1];
-    
-    if (liveHour == hourdiv) {
-      $(this).addClass("now");
-      $(this).children(".description").addClass("whitefont");
-    } else if (liveHour < hourdiv) {
-      $(this).removeClass("now");
-      $(this).addClass("forward");
-    } else if (liveHour > hourdiv) {
-      $(this).removeClass("forward");
-      $(this).addClass("back");
+  //check each hour block and change the background to the relative colour
+$.each(timeBlock, function () {
+    var hourId = parseInt($(this).attr("id"));
+    console.log(liveHour)
+    console.log(parseInt($(this).attr("id")));
+    // for events at the current hour, set the colour to red.
+    if (hourId === liveHour) {
+        $(this).next().addClass("now");
+        console.log("1")
+    // for past events set the colour to grey
+    } else if (hourId < liveHour) {
+        $(this).next().addClass("back");
+        console.log("2")
+    // for future events set the colour to green
+    } else if (hourId > liveHour) {
+        $(this).next().addClass("forward");
+        console.log("3")
     }
-  });
+});
 
-  $(".btntosave").click(function (event) {
-    event.preventDefault();
-    var value = $(this).child(".the-hour").val();
-    var time = $(this).parent().attr("id").split("-")[1];
+  // $(".timeinrow").each(function () {
+  //   var hourdiv = $(this).attr("id").split("-")[1];
+    
+  //   if (liveHour == hourdiv) {
+  //     $(this).addClass("now");
+  //     $(this).children(".description").addClass("whitefont");
+  //   } else if (liveHour < hourdiv) {
+  //     $(this).removeClass("now");
+  //     $(this).addClass("forward");
+  //   } else if (liveHour > hourdiv) {
+  //     $(this).removeClass("forward");
+  //     $(this).addClass("back");
+  //   }
+  // });
+
+  // $(".saveInfoButton").click(function (event) {
+  //   event.preventDefault();
+  //   var value = $(this).siblings(".itemforthehour").val();
+  //   var time = $(this).parent().attr("id").split("-")[1];
+  //   localStorage.setItem(time, value);
+  // });
+
+  $(".saveInfoButton").on("click", function(){
+
+    var value = $(this).siblings(".itemforthehour").val();
+    var time = $(this).parent().attr("id");
+    var saved = $("#saved");
+    console.log(time + " " + value);
     localStorage.setItem(time, value);
-  });
 
+//     document.getElementById("saved").innerHTML = "Saved!";
 
-  $("#0900AM .the-hour").val(localStorage.getItem("9h"));
-  $("#1000AM .the-hour").val(localStorage.getItem("10h"));
-  $("#1100AM .the-hour").val(localStorage.getItem("11h"));
-  $("#1200PM .the-hour").val(localStorage.getItem("12h"));
-  $("#1300PM .the-hour").val(localStorage.getItem("13h"));
-  $("#1400PM .the-hour").val(localStorage.getItem("14h"));
-  $("#1500PM .the-hour").val(localStorage.getItem("15h"));
-  $("#1600PM .the-hour").val(localStorage.getItem("16h"));
-  $("#1700PM .the-hour").val(localStorage.getItem("17h"));
+// setTimeout(function(){
+//     document.getElementById("saved").innerHTML = '';
+// }, 3000);
+    
+// save text area into the local storage
+$(".saveInfoButton").on("click", function (event) {
+  var calendarItem =
+      event.target.parentElement.previousElementSibling.children[0].value;
+  localStorage.setItem(event.target.attributes[0].value, calendarItem);
+});
+    
+    });
+
+  
 });
